6e8f4f47926c489be8882588139841e50b9be4d4
Robert Selvek <robert.selvek@gmail.com>
2012-05-14 18:06:47 +0200

    Change indentation
    Before, the .cabal file was indented using spaces, and code files with
    tabs. Now everything's using spaces.

--- src/XKCD.hs
+++ src/XKCD.hs
@@ -12,19 +12,19 @@ downloadXKCD path = getImageURL >>= downloadFile >>= B.writeFile path
 -- |Gets the URL of the most recent xkcd comic strip
 getImageURL :: IO String
 getImageURL = do
-	tags <- fmap parseTags $ openURL "http://xkcd.com"
-	return $ fromJust (getImageString $ getImageTag tags)
-	where
-		getImageTag :: [Tag String] -> Tag String
-		getImageTag tags = (dropWhile (/= TagOpen "div" [("id", "comic")]) tags) !! 2
+  tags <- fmap parseTags $ openURL "http://xkcd.com"
+  return $ fromJust (getImageString $ getImageTag tags)
+  where
+    getImageTag :: [Tag String] -> Tag String
+    getImageTag tags = (dropWhile (/= TagOpen "div" [("id", "comic")]) tags) !! 2
 
 -- |Searches for the first @img@ tag in a list and returns its @src@ attribute
 getImageString :: Tag String -> Maybe String
 getImageString (TagOpen "img" attrs) = foldl getAttr Nothing attrs
-	where	
-		getAttr Nothing ("src", src) = Just src
-		getAttr (Just x) (_, _) = Just x
-		getAttr Nothing (_, _) = Nothing
+  where 
+    getAttr Nothing ("src", src) = Just src
+    getAttr (Just x) (_, _) = Just x
+    getAttr Nothing (_, _) = Nothing
 getImageString _ = Nothing
 
 -- |Opens URL and gets the body of response
@@ -34,6 +34,6 @@ openURL x = getResponseBody =<< simpleHTTP (getRequest x)
 -- |Downloads file from URL and returns its contents as a 'B.ByteString'
 downloadFile :: String -> IO B.ByteString
 downloadFile url = response >>= getResponseBody
-	where 
-		response = simpleHTTP ((mkRequest GET $ fromJust $ parseURI url) :: Request B.ByteString)
+  where 
+    response = simpleHTTP ((mkRequest GET $ fromJust $ parseURI url) :: Request B.ByteString)
 
--- src/main.hs
+++ src/main.hs
@@ -11,23 +11,23 @@ main = getPath >>= downloadXKCD
 -- |Gets the into which it should download the comic
 getPath :: IO FilePath
 getPath = do
-	path <- processArgs
-	case path of 
-		Just path -> return path 
- 		Nothing -> fmap (++ "/strip.jpg") $ getCurrentDirectory
+  path <- processArgs
+  case path of 
+    Just path -> return path 
+    Nothing -> fmap (++ "/strip.jpg") $ getCurrentDirectory
 
 -- |Checks if the arguments contain a path, or help flag. If they do, prints the USAGE message
 processArgs :: IO (Maybe FilePath)
 processArgs = do
-	args <- getArgs
- 	case args of
- 		[] -> return Nothing
- 		("-h":_) -> printHelp
- 		("--help":_) -> printHelp
- 		path -> return $ Just . head $ path
- 	where printHelp = do
- 		putStrLn help
- 		exitSuccess
+  args <- getArgs
+  case args of
+    [] -> return Nothing
+    ("-h":_) -> printHelp
+    ("--help":_) -> printHelp
+    path -> return $ Just . head $ path
+  where printHelp = do
+    putStrLn help
+    exitSuccess
 
 -- |USAGE string
 help :: String
